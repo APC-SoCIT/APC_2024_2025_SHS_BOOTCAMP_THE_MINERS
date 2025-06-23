36f3ffb663cc4ac190bbf324af27c4632d7f6567
Kurt Maquee <kdferrer@student.apc.edu.ph>
2025-06-24 00:23:58 +0800

    big ui change

--- RAMBLE_v1/RAMble/admin.py
+++ RAMBLE_v1/RAMble/admin.py
@@ -6,3 +6,6 @@ admin.site.register(Tutor)
 admin.site.register(Subject)
 admin.site.register(TutorAvailability)
 
+class TutorAdmin(admin.ModelAdmin):
+    list_display = ('full_name', 'email', 'rating', 'rate_per_hour')
+    fields = ('full_name', 'email', 'rating', 'rate_per_hour', 'subjects', 'about', 'profile_picture')
--- RAMBLE_v1/ramble_backend/settings.py
+++ RAMBLE_v1/ramble_backend/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -134,3 +135,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = '/'
 LOGIN_URL = '/login/'  # Redirects to login page if unauthenticated
 LOGOUT_REDIRECT_URL = '/'  # Redirects to homepage after logout
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # For collectstatic
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
